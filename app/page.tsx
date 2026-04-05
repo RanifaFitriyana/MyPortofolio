f1a40a4601514f7b9c78b89dfd6746679e7e4b6a
RanifaFitriyana <fitriyanaranifa@gmail.com>
2026-04-05 13:02:45 +0700

    add image profile

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Navbar from "../components/Navbar";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -9,8 +10,15 @@ export default function Home() {
       
       {/* HERO */}
       <section id="home" className="flex flex-col items-center justify-center h-screen text-center">
+        <Image
+          src="/IMG_5906-removebg-preview.png"
+          alt="Profile"
+          width={200}
+          height={200}
+          className="rounded-full shadow-xl hover:scale-105 transition mb-5"
+        />
         <h1 className="text-5xl font-bold">
-          Halo, saya <span className="text-blue-500">Ranifa</span>
+          Halo, saya <span className="text-blue-800">Ranifa</span>
         </h1>
         <p className="mt-4 text-gray-400 max-w-xl">
           Saya seorang Web Developer yang fokus pada React, Next.js, dan TypeScript.
@@ -19,25 +27,46 @@ export default function Home() {
 
       {/* PROJECTS */}
       <section id="projects" className="py-20">
-        <h2 className="text-3xl font-semibold mb-8">Projects</h2>
+        <h2 className="text-3xl font-semibold mb-8 text-center">Projects</h2>
 
         <div className="grid md:grid-cols-3 gap-6">
-          <div className="p-6 bg-gray-900 rounded-3xl">
-            <h3 className="text-xl font-bold">Project 1</h3>
+          <div className="p-6 bg-white border border-blue-800 shadow-md hover:shadow-xl hover:scale-105 transition-all duration-300 rounded-3xl">
+            <h3 className="text-xl font-bold text-center">Project 1</h3>
             <p className="text-gray-400 mt-2">
               Deskripsi singkat project kamu.
             </p>
           </div>
 
-          <div className="p-6 bg-gray-900 rounded-3xl">
-            <h3 className="text-xl font-bold">Project 2</h3>
+          <div className="p-6 bg-white border border-blue-800 shadow-md hover:shadow-xl hover:scale-105 transition-all duration-300 rounded-3xl">
+            <h3 className="text-xl font-bold text-center">Project 2</h3>
             <p className="text-gray-400 mt-2">
               Deskripsi singkat project kamu.
             </p>
           </div>
 
-          <div className="p-6 bg-gray-900 rounded-3xl">
-            <h3 className="text-xl font-bold">Project 3</h3>
+          <div className="p-6 bg-white border border-blue-800 shadow-md hover:shadow-xl hover:scale-105 transition-all duration-300 rounded-3xl">
+            <h3 className="text-xl font-bold text-center">Project 3</h3>
+            <p className="text-gray-400 mt-2">
+              Deskripsi singkat project kamu.
+            </p>
+          </div>
+
+          <div className="p-6 bg-white border border-blue-800 shadow-md hover:shadow-xl hover:scale-105 transition-all duration-300 rounded-3xl">
+            <h3 className="text-xl font-bold text-center">Project 1</h3>
+            <p className="text-gray-400 mt-2">
+              Deskripsi singkat project kamu.
+            </p>
+          </div>
+
+          <div className="p-6 bg-white border border-blue-800 shadow-md hover:shadow-xl hover:scale-105 transition-all duration-300 rounded-3xl">
+            <h3 className="text-xl font-bold text-center">Project 2</h3>
+            <p className="text-gray-400 mt-2">
+              Deskripsi singkat project kamu.
+            </p>
+          </div>
+
+          <div className="p-6 bg-white border border-blue-800 shadow-md hover:shadow-xl hover:scale-105 transition-all duration-300 rounded-3xl">
+            <h3 className="text-xl font-bold text-center">Project 3</h3>
             <p className="text-gray-400 mt-2">
               Deskripsi singkat project kamu.
             </p>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,16 @@
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 w-full bg-blue backdrop-blur border-b border-blue-800 z-50">
+    <nav className="fixed top-0 left-0 w-full bg-white/80 backdrop-blur border-b border-blue-800 z-50">
       <div className="max-w-5xl mx-auto px-6 py-4 flex justify-between items-center">
         
         {/* Logo */}
-        <h1 className="font-bold text-lg">MyPortfolio</h1>
+        <h1 className="font-bold text-lg text-black">Portfolio</h1>
 
         {/* Menu */}
-        <div className="flex gap-6 text-sm">
-          <a href="#home" className="text-blue hover:text-white">Home</a>
-          <a href="#projects" className="text-blue hover:text-white">Projects</a>
-          <a href="#contact" className="text-blue hover:text-white">Contact</a>
+        <div className="flex gap-6 text-md">
+          <a href="#home" className="text-black hover:text-blue-900">Home</a>
+          <a href="#projects" className="text-black hover:text-blue-900">Projects</a>
+          <a href="#contact" className="text-black hover:text-blue-900">Contact</a>
         </div>
 
       </div>
